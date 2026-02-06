ea8407a2525d160a265c14ad287e669b7d3f3dc7
sora0319 <sojeoung536985@gmail.com>
2026-02-07 07:39:53 +0900

    feat : 예외 핸들러 처리 기능 추가

--- src/main/java/com/server/hackathon/common/GlobalExceptionHandler.java
+++ src/main/java/com/server/hackathon/common/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import software.amazon.awssdk.services.s3.model.S3Exception;
 
 @Slf4j
 @RestControllerAdvice // 모든 컨트롤러에서 발생하는 예외를 여기서 잡습니다.
@@ -18,15 +17,6 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getStatus())
                 .body(ResponseDto.fail(e.getMessage()));
-    }
-
-    // s3 관련 에러
-    @ExceptionHandler(S3Exception.class)
-    public ResponseEntity<ResponseDto<Void>> handleS3Exception(S3Exception e) {
-        log.error("S3 AWS Error: {}", e.awsErrorDetails().errorMessage());
-        return ResponseEntity
-                .status(e.statusCode()) // AWS가 준 상태 코드 그대로 사용
-                .body(ResponseDto.fail("S3 연동 중 오류가 발생했습니다: " + e.awsErrorDetails().errorMessage()));
     }
 
     // 그외 오류
