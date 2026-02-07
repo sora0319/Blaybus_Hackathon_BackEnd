9c3cb4611033bdb43b4f8e0b400d822716001d4b
sora0319 <sojeoung536985@gmail.com>
2026-02-07 20:16:56 +0900

    refactor : 사용자 엔티티에 base entity 적용

--- src/main/java/com/server/hackathon/member/client/model/Users.java
+++ src/main/java/com/server/hackathon/member/client/model/Users.java
@@ -1,23 +1,21 @@
 package com.server.hackathon.member.client.model;
 
+import com.server.hackathon.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import java.util.UUID;
-import java.util.Base64;
 
 @Entity
 @Getter
 @Table(name = "users")
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
-public class Users {
+public class Users extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,24 +30,6 @@ public class Users {
 
     @Column(nullable = false, unique = true)
     private String email;
-
-    @Column(nullable = false, unique = true, length = 25)
-    private String shortUuid;
-
-    // 엔티티가 UUID를 생성하고 짧게 변환
-    @PrePersist
-    public void createShortUuid() {
-        UUID uuid = UUID.randomUUID();
-        // UUID의 byte 배열을 Base64로 인코딩하여 길이를 단축 (약 22자)
-        this.shortUuid = Base64.getUrlEncoder().withoutPadding().encodeToString(asBytes(uuid));
-    }
-
-    private static byte[] asBytes(UUID uuid) {
-        java.nio.ByteBuffer bb = java.nio.ByteBuffer.wrap(new byte[16]);
-        bb.putLong(uuid.getMostSignificantBits());
-        bb.putLong(uuid.getLeastSignificantBits());
-        return bb.array();
-    }
 
     @Builder
     private Users(String username, String password, String email){
