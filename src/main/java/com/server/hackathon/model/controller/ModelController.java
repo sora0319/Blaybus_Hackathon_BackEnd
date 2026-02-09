774a3316e41d2c3ca3abfeefcc513395f1233559
sora0319 <sojeoung536985@gmail.com>
2026-02-09 23:20:06 +0900

    feat : 모델 설명 및 부품 url 전달 기능 생성

--- src/main/java/com/server/hackathon/model/controller/ModelController.java
+++ src/main/java/com/server/hackathon/model/controller/ModelController.java
@@ -1,11 +1,13 @@
 package com.server.hackathon.model.controller;
 
 import com.server.hackathon.common.ResponseDto;
+import com.server.hackathon.model.controller.dto.ModelDetailResponse;
 import com.server.hackathon.model.controller.dto.ModelsResponseDto;
 import com.server.hackathon.model.service.ModelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +29,15 @@ public class ModelController {
 
         // 공통 ResponseDto로 감싸서 반환
         return ResponseDto.success("사용자 모델 목록 조회 성공", models);
+    }
+
+    @GetMapping("/model/{modelUuid}")
+    public ResponseDto<ModelDetailResponse> getModelDetail(
+            @AuthenticationPrincipal String memberUuid,
+            @PathVariable String modelUuid
+    ) {
+        ModelDetailResponse response = modelService.getModelDetail(memberUuid, modelUuid);
+
+        return ResponseDto.success("모델 상세 정보 조회 성공", response);
     }
 }
