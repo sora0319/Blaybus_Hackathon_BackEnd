1526006282a6390e57e4d4035306bc58f08945f1
sora0319 <sojeoung536985@gmail.com>
2026-02-09 22:33:20 +0900

    feat : 모델 정보 및 부품 정보 삽입하는 기능들 추가

--- src/main/java/com/server/hackathon/model/repository/ModelRepository.java
+++ src/main/java/com/server/hackathon/model/repository/ModelRepository.java
@@ -1,8 +1,9 @@
 package com.server.hackathon.model.repository;
 
 import com.server.hackathon.model.model.Model;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ModelRepository extends JpaRepository<Model, Long> {
-
+    List<Model> findAll();
 }
